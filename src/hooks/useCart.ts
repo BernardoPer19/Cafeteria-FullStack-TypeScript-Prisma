b6e32d0788332a82aa5aa4741b5c9645c1b54ee8
jhoaking <peredoroj@gmail.com>
2025-01-27 17:29:30 -1100

    prueba

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,3 +1,9 @@
+import { useState } from "react"
+
 const useCart = () => {
-    
+    const [e, sets] = useState("second")
+    const [h, seth] = useState("second")
+    const [ca, ser324] = useState("second")
+    const [q, setsz] = useState("second")
+    const [a, setxv] = useState("second")
 }
