93728d984af62e34e5597e8347867a6dada9d362
b.eww_ <148910513+BernardoPer19@users.noreply.github.com>
2025-01-29 09:55:34 -1100

    Update Header.tsx

--- src/home/Header.tsx
+++ src/home/Header.tsx
@@ -10,10 +10,10 @@ import React from "react";
             "url('https://images.unsplash.com/photo-1497935586351-b67a49e012bf?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
         }}
       >
-        {/* Capa oscura */}
+       
         <div className="absolute inset-0 bg-black/50"></div>
 
-        {/* Contenido del Header */}
+   
         <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-6">
           <h1 className="text-5xl font-bold mb-4 leading-tight">
             Bienvenidos a nuestra Cafetería
@@ -24,7 +24,7 @@ import React from "react";
             experiencia!
           </p>
 
-          {/* Botón de llamada a la acción */}
+         
           <Link
             href="#menu"
             className="bg-yellow-500 text-black py-3 px-8 rounded-full text-lg font-semibold transition duration-300 hover:bg-yellow-400"
