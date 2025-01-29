46250a659b13a95ec28de97e4e6b7f8141d51b00
b.eww_ <148910513+BernardoPer19@users.noreply.github.com>
2025-01-29 09:54:24 -1100

    Update FormatDates.ts

--- src/utils/FormatDates.ts
+++ src/utils/FormatDates.ts
@@ -1,12 +1,12 @@
 export const formatDate = (dateString: string): string => {
     const date = new Date(dateString);
     return date.toLocaleString("es-ES", {
-      weekday: "long", // Día de la semana
+      weekday: "long", 
       year: "numeric", 
       month: "long", 
       day: "numeric",
       hour: "numeric", 
       minute: "numeric", 
-      hour12: true, // Si deseas usar el formato de 12 horas (AM/PM)
+      hour12: true, 
     });
   };
